0a5a69e3871f813aa1570a6882537561f47bc03a
irisgao713 <irisco226@yahoo.com>
2020-01-16 14:41:17 -0800

    mini program to move htmls

---
+++ movefile.py
@@ -0,0 +1,12 @@
+import shutil, os, glob
+ 
+srcDir = ''
+dstDir = '../results/raw_html/2020-01-16'
+path = srcDir + '/*.{}'
+if os.path.isdir(srcDir) and os.path.isdir(dstDir) :
+        # Iterate over all the files in source directory
+    for filePath in glob.glob(path.format('html')):
+            # Move each file to destination Directory
+        shutil.move(filePath, dstDir)
+else:
+    print("srcDir & dstDir should be Directories")
