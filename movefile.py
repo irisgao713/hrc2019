a44bf636caac79e6a4b76d91d42bfdda68abc4a6
irisgao713 <irisco226@yahoo.com>
2020-01-16 14:46:21 -0800

    no /

--- movefile.py
+++ movefile.py
@@ -2,7 +2,7 @@ import shutil, os, glob
  
 srcDir = ''
 dstDir = '../results/raw_html/2020-01-16'
-path = srcDir + '/*.{}'
+path = srcDir + '*.{}'
 if os.path.isdir(dstDir) :
         # Iterate over all the files in source directory
     for filePath in glob.glob(path.format('html')):
