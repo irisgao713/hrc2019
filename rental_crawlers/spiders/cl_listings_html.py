9044d7ff151411f1b0c289b680cbd0369f95f0cf
irisgao713 <irisco226@yahoo.com>
2020-01-21 12:52:57 -0800

    remove roo for html crawling

--- rental_crawlers/spiders/cl_listings_html.py
+++ rental_crawlers/spiders/cl_listings_html.py
@@ -14,7 +14,7 @@ class CLWebSpider(CrawlSpider):
     allowed_domains = ['vancouver.craigslist.org']
     start_urls = [
         'https://vancouver.craigslist.org/search/apa'
-        'https://vancouver.craigslist.org/d/rooms-shares/search/roo'
+        #'https://vancouver.craigslist.org/d/rooms-shares/search/roo'
     ]
     #rooms for rent 'https://vancouver.craigslist.ca/rds/roo/' 
     #apartments 'https://vancouver.craigslist.ca/search/rds/apa'
@@ -22,12 +22,11 @@ class CLWebSpider(CrawlSpider):
     Rules for automatically following the links to the listing, and going to the next listing. 
     '''
     rules = (
-        #Rule(LinkExtractor(allow=(), restrict_xpaths=('//a[@class="result-title hdrlnk"]')), follow=True, callback='archive_listings'),
-        Rule(LinkExtractor(allow=(), restrict_xpaths=('//a[contains(@class, "button next")]')), follow=True, callback='archive_listings'),
-        
-        Rule(LinkExtractor(allow=(), restrict_xpaths=('//a[contains(@class, "next")]')), follow=True, callback='archive_listings'),
-   
-        Rule(LinkExtractor(allow=(), restrict_xpaths=('//ul[@class="rows"]/li[1]/a')), follow=True, callback='parse_listings')
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//a[@class="result-title hdrlnk"]')), follow=True, callback='parse_listings'),
+       # Rule(LinkExtractor(allow=(), restrict_xpaths=('//a[contains(@class, "button next")]')), follow=True, callback='parse_listings')
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//a[contains(@class, "button next")]')), follow=True, callback='parse_listings'),
+        #Rule(LinkExtractor(allow=(), restrict_xpaths=('//a[contains(@class, "next")]')), follow=True, callback='archive_listings'),
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//ul[@class="rows"]/li[@class="result-row"]/a')), follow=True, callback='parse_listings')
     )
 
     custom_settings = {
