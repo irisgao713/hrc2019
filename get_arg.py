e342357830e389fda35e54cd79449af0fdf5560e
irisgao713 <irisco226@yahoo.com>
2020-01-30 17:04:17 -0800

    put in getPath function

--- get_arg.py
+++ get_arg.py
@@ -30,3 +30,35 @@ def getArg(fullCmdArguments):
             directory = str(currentValue)
 
     return mode, directory
+
+
+def getPath(fullCmdArguments):
+# read commandline arguments, first
+#fullCmdArguments = sys.argv
+
+    # - further arguments
+    argumentList = fullCmdArguments[1:]
+
+    unixOptions = "fd"
+    gnuOptions = ["file=", "dir="]
+
+    try:
+        arguments, values = getopt.getopt(argumentList, unixOptions, gnuOptions)
+    except getopt.error as err:
+        # output error, and return with an error code
+        print (str(err))
+        sys.exit(2)
+
+    #initiate optional arguments
+    directory = ''
+    file = ''
+    # evaluate given options
+    for currentArgument, currentValue in arguments,values:
+        if currentArgument in ("-f", "--file"):
+            print (("Pointing source file to: %s") % (currentValue))
+            file = str(currentValue)
+        elif currentArgument in ("-d", "--dir"):
+            print (("Pointing source directory to: %s") % (currentValue))
+            directory = str(currentValue)
+
+    return file, directory
