887dac32f1a84f412a749df2920b17263f039e38
irisgao713 <irisco226@yahoo.com>
2020-02-26 17:10:15 -0800

    separate delta enabled mode and normal mode

--- rental_crawlers/spiders/cl_listings_html.py
+++ rental_crawlers/spiders/cl_listings_html.py
@@ -8,7 +8,9 @@ from scrapy.linkextractors import LinkExtractor
 import hashlib
 import datetime
 
-class CLWebSpider(CrawlSpider):
+
+
+class DeltaCLWebSpider(CrawlSpider):
 
     name = 'cl_listings_html'
     allowed_domains = ['vancouver.craigslist.org']
@@ -34,6 +36,78 @@ class CLWebSpider(CrawlSpider):
         'DELTAFETCH_ENABLED': True,
         'SPIDER_MIDDLEWARES': {
             'scrapy_deltafetch.DeltaFetch': 120,
+            'scrapy.spidermiddlewares.offsite.OffsiteMiddleware': None
+        },
+        'ITEM_PIPELINES' : {
+            'rental_crawlers.pipelines.CLPipeline': 300,
+        }
+    }
+
+    '''
+    Callback method for parsing the response text into a CLItem. 
+    '''
+    # def archive_listings(self, res):
+    #     month = datetime.date.today().strftime("%Y-%m-%d")
+
+    #     folder = "../results/raw_html/" + month 
+    #     # if not os.path.exists(folder):
+    #     #     os.mkdir(folder)
+
+    #     path = folder + "/" + self.hash_url(res.url)
+        
+    #     with open(path, 'w+') as f:
+    #         f.write(res.body)
+    #         f.close
+
+
+    def parse_listings(self, response):
+
+        hashed_url = self.hash_url(response.url)   
+        with open(hashed_url, 'w+') as f:
+            f.write(response.body.decode("utf-8"))
+            f.close()
+        
+        yield {
+            'url': hashed_url
+        }
+
+
+
+    '''
+    To generate file name from url instead of using url as file name
+    '''
+    def hash_url(self,url):
+        return hashlib.sha224(str(url).encode('utf-8')).hexdigest()+'.html'
+
+
+
+
+
+class CLWebSpider(CrawlSpider):
+
+    name = 'cl_listings_html'
+    allowed_domains = ['vancouver.craigslist.org']
+    start_urls = [
+        'https://vancouver.craigslist.org/search/apa'
+        #'https://vancouver.craigslist.org/d/rooms-shares/search/roo'
+    ]
+    #rooms for rent 'https://vancouver.craigslist.ca/rds/roo/' 
+    #apartments 'https://vancouver.craigslist.ca/search/rds/apa'
+    '''
+    Rules for automatically following the links to the listing, and going to the next listing. 
+    '''
+    rules = (
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//a[@class="result-title hdrlnk"]')), follow=True, callback='parse_listings'),
+       # Rule(LinkExtractor(allow=(), restrict_xpaths=('//a[contains(@class, "button next")]')), follow=True, callback='parse_listings')
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//a[@class ="button next"]')), follow=True, callback='parse_listings'),
+        #Rule(LinkExtractor(allow=(), restrict_xpaths=('//a[contains(@class, "next")]')), follow=True, callback='archive_listings'),
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//ul[@class="rows"]/li[@class="result-row"]/a')), follow=True, callback='parse_listings')
+    )
+
+    custom_settings = {
+        'LOG_LEVEL': 'DEBUG',
+        'DELTAFETCH_ENABLED': False,
+        'SPIDER_MIDDLEWARES': {
             'scrapy.spidermiddlewares.offsite.OffsiteMiddleware': None
         },
         'ITEM_PIPELINES' : {
--- rental_crawlers/spiders/cl_listings_roo.py
+++ rental_crawlers/spiders/cl_listings_roo.py
@@ -5,6 +5,83 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from rental_crawlers.items import CLItem
 
+
+
+class DeltaCLROOSpider(CrawlSpider):
+
+    name = 'cl_listings'
+    allowed_domains = ['vancouver.craigslist.org']
+    start_urls = [
+        'https://vancouver.craigslist.org/d/rooms-shares/search/roo'
+    ]
+
+    '''
+    Rules for automatically following the links to the listing, and going to the next listing. 
+    '''
+    rules = (
+        #Rule(LinkExtractor(allow=(), restrict_xpaths=('//a[@class="result-title hdrlnk"]')), follow=True, callback='parse_listings'),
+        #Rule(LinkExtractor(allow=(), restrict_xpaths=('//a[contains(@class, "button next")]')), follow=True, callback='parse_listings')
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//a[@class ="button next"]')), follow=True, callback='parse_listings'),
+        #Rule(LinkExtractor(allow=(), restrict_xpaths=('//a[contains(@class, "next")]')), follow=True, callback='archive_listings'),
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//ul[@class="rows"]/li[@class="result-row"]/a')), follow=True, callback='parse_listings')
+    
+    )
+
+    custom_settings = {
+        'LOG_LEVEL': 'DEBUG',
+        'DELTAFETCH_ENABLED': True,
+        'SPIDER_MIDDLEWARES': {
+            'scrapy_deltafetch.DeltaFetch': 120,
+            'scrapy.spidermiddlewares.offsite.OffsiteMiddleware': None
+        },
+        'ITEM_PIPELINES' : {
+            'rental_crawlers.pipelines.CLPipeline': 300,
+        }
+    }
+
+
+
+    '''
+    Callback method for parsing the response text into a CLItem. 
+    '''
+    def parse_listings(self, response):
+        item = CLItem()
+
+        item['title'] = response.xpath('//span[@id="titletextonly"]/text()').extract_first()
+        item['location'] = response.xpath('//small/text()').extract_first()
+        item['sqft'] = response.xpath('//span[@class="housing"]/text()').extract_first()
+        item['price'] = response.xpath('//span[@class="price"]/text()').extract_first()
+        item['date'] = response.xpath('//time/@datetime').extract_first()
+        item['lat'] = response.xpath('//div/@data-latitude').extract_first()
+        item['long'] = response.xpath('//div/@data-longitude').extract_first()
+        #item['description'] = response.xpath('//section[@id="postingbody"]/text()').extract_first()
+        item['description'] = response.xpath('string(//section[@id="postingbody"])').extract()
+        
+        item['url'] = response.url
+        item['source'] = "Craigslist"
+        item['domain'] = response.xpath('//section/header[1]/nav/ul/li[2]/p/a/text()').extract_first()
+        #NEW ITEMS
+
+        item['location_accuracy'] = response.xpath('//div/@data-accuracy').extract_first()
+        item['num_of_images']= len(response.xpath('//div[@class = "swipe-wrap"]/div').extract())
+
+        map_address = response.xpath('//div[@class="mapaddress"]/text()')
+        
+        t1= response.xpath('//p[@class = "attrgroup"]/span[@class = "shared-line-bubble"]/b/text()').extract()
+        t2 = response.xpath('//p[@class = "attrgroup"]/span/text()').extract()
+        item['tags']= t1+t2
+
+        if not len(map_address) < 1:
+            item['map_address'] = map_address.extract_first()
+        
+        # if not len(tags) < 1 and not len(t2) < 1:
+            # item['tags'] = tags.extract() 
+        
+        yield item
+
+
+
+
 class CLROOSpider(CrawlSpider):
 
     name = 'cl_listings'
@@ -29,7 +106,6 @@ class CLROOSpider(CrawlSpider):
         'LOG_LEVEL': 'DEBUG',
         'DELTAFETCH_ENABLED': False,
         'SPIDER_MIDDLEWARES': {
-            #'scrapy_deltafetch.DeltaFetch': 120,
             'scrapy.spidermiddlewares.offsite.OffsiteMiddleware': None
         },
         'ITEM_PIPELINES' : {
