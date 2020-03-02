f204ee8219a4ef153380230d3a11bf944f2872d8
irisgao713 <irisco226@yahoo.com>
2020-03-02 15:48:03 -0800

    define separate processes for apa and roo archiving

--- run_spiders.py
+++ run_spiders.py
@@ -24,7 +24,56 @@ month = datetime.date.today().strftime("%Y-%m")
 
 mode, directory = getArg(sys.argv)
 
-if mode == 'archive':
+if mode =='archive':
+
+    ### apa
+    process1 = CrawlerProcess({
+            'USER_AGENT': default_settings.USER_AGENT,
+            'FEED_FORMAT': 'csv',
+            'FEED_URI': "../results/parsed_raw/apa/listings-" + month + ".csv"
+        })
+        
+    extension = 'html'
+    os.chdir('../results') 
+    #path = "../results/raw_html/" + directory +'/*.{}'
+    #if not os.path.exists("../results/raw_html/" + directory):
+    path = "raw_html/apa/" + directory +'/*.{}'
+    if not os.path.exists("raw_html/apa/" + directory):
+        print('The directory: <' + str(directory) +'> does not exist in '+ "raw_html/apa" )
+        exit 
+
+    prefix  = os.getcwd()
+    all_filenames = ['file://' + prefix + '/' + i for i in glob.glob(path.format(extension))]
+
+    
+    process1.crawl(CLLSpider,start_urls = all_filenames)
+
+
+
+    #### roo
+    process2 = CrawlerProcess({
+            'USER_AGENT': default_settings.USER_AGENT,
+            'FEED_FORMAT': 'csv',
+            'FEED_URI': "../results/parsed_raw/roo/listings-" + month + ".csv"
+        })
+    
+    extension = 'html'
+    os.chdir('../results') 
+    #path = "../results/raw_html/" + directory +'/*.{}'
+    #if not os.path.exists("../results/raw_html/" + directory):
+    path = "raw_html/roo/" + directory +'/*.{}'
+    if not os.path.exists("raw_html/roo/" + directory):
+        print('The directory: <' + str(directory) +'> does not exist in '+ "raw_html/roo" )
+        exit 
+
+    prefix  = os.getcwd()
+    all_filenames = ['file://' + prefix + '/' + i for i in glob.glob(path.format(extension))]
+
+    
+    process2.crawl(CLLSpider,start_urls = all_filenames)
+
+
+elif mode == 'archiveOLD':
     # FEED_FORMAT is the output file type (accepts csv, json)
     # FEED_URI is the name of the output file (if no path specified, will put in same folder as where script is)
     for ad_type in ['apa','roo']:
@@ -54,9 +103,9 @@ if mode == 'archive':
         # process.crawl(KJSpider)
         # Need Splash running for VSpider: docker run -p 8050:8050 -p 5023:5023 scrapinghub/splash
         # process.crawl(VSpider)
-        process.start()
-        process.addBoth(lambda _: reactor.stop())
-        reactor.run()
+        # process.start()
+        # process.addBoth(lambda _: reactor.stop())
+        # reactor.run()
 
 
 elif mode == 'web':
