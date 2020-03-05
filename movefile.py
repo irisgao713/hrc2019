d97dcc69295a25ff23192b8713b7d35aed318910
irisgao713 <irisco226@yahoo.com>
2020-03-04 22:24:59 -0800

    use absolute path to replace exsiting file

--- movefile.py
+++ movefile.py
@@ -3,17 +3,18 @@ import os
 import shutil
 
 
-os.chdir('../results/raw_html/roo/') 
+os.chdir('../results') 
 def movefile(dstDir):
     srcDir = ''
     path = srcDir + '*.{}'
     if os.path.isdir(dstDir) :
         # Iterate over all the files in source directory
         for filePath in glob.glob(path.format('html')):
-            # Move each file to destination Directory
-            shutil.move(filePath, dstDir)
+            # Move each file to destination Directory, use absolute path to replace existing
+            dst = dstDir + "/" + filePath
+            shutil.move(os.path.abspath(filePath), os.path.abspath(dst))
     else:
         'Can not move htmls to designated folder'
 
 
-movefile("2020-02")
+movefile("../results/raw_html/roo/2020-02")
