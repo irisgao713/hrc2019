bf666b65c9e0ccf73e872966e585f9dda8a5bbbc
irisgao713 <irisco226@yahoo.com>
2020-04-30 16:51:14 -0700

    added ranint to generate random minutes

--- scheduler.py
+++ scheduler.py
@@ -21,14 +21,15 @@ from crawlers.spiders.cl_listings_roo import RooSpider, DeltaRooSpider
 from crawlers.spiders.cl_listings_local import CLLSpider
 
 
-'''
-Define a CrawlerProcess
-    : param ad_type: determines the path for 'FEED_URI'. Only accepts either 'apa' or 'roo'.
-    :return: process -- a CrawlerProcess
 
-'''
 
 def ArchiveProcess(ad_type):
+    '''
+    Define a CrawlerProcess
+        : param ad_type: determines the path for 'FEED_URI'. Only accepts either 'apa' or 'roo'.
+        :return: process -- a CrawlerProcess
+
+    '''
     month = datetime.date.today().strftime("%Y-%m") 
     process = CrawlerProcess({
             'USER_AGENT': default_settings.USER_AGENT,
@@ -38,18 +39,19 @@ def ArchiveProcess(ad_type):
     return process
 
 
-'''
-Find a list of HTML file path for the archive spiders to crawl. The function should return all HTML file path (of a specific ad type)
-that are scraped during the previous month and stored locally. 
-
-For example, if the current month is April 2020, and ad_type = 'apa', then the function will return all HTML paths in: '../results/raw_html/apa/2020-03'.
-
-    : param ad_type: determines the path of the folder to look for HTML files. Only accepts either 'apa' or 'roo'.
-    :return: all_filenames: a list of html file paths in the folder specified by 'ad_type' and current month
-'''     
+   
 
 def getFiles(ad_type):
     # Get directory in the form of yy-mm but for the previous month
+    '''
+    Find a list of HTML file path for the archive spiders to crawl. The function should return all HTML file path (of a specific ad type)
+    that are scraped during the previous month and stored locally. 
+
+    For example, if the current month is April 2020, and ad_type = 'apa', then the function will return all HTML paths in: '../results/raw_html/apa/2020-03'.
+
+        : param ad_type: determines the path of the folder to look for HTML files. Only accepts either 'apa' or 'roo'.
+        :return: all_filenames: a list of html file paths in the folder specified by 'ad_type' and current month
+    '''  
     month = datetime.date.today().month 
     if month == 1:
         directory = str(datetime.date.today().year) + '-12' 
@@ -74,7 +76,11 @@ def getFiles(ad_type):
     return all_filenames
 
 
-
+def rand_min():
+    '''
+    Return a random number between 0 and 60
+    '''
+    return random.randint(0,60)
 
 if __name__ == '__main__':
     print('Reminder: Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
@@ -87,19 +93,19 @@ if __name__ == '__main__':
 
     process = CrawlerProcess()
     ## On the first day of each month, disable and reset scrapy deltafetch, retrieve all available ads on the domain
-    scheduler.add_job(process.crawl, 'cron', args =[ApaSpider], day='1', hour=8, minute=30)
-    scheduler.add_job(process.crawl,'cron', args=[RooSpider], day='1', hour=8, minute=0)
+    scheduler.add_job(process.crawl, 'cron', args =[ApaSpider], day='1', hour=8, minute=rand_min())
+    scheduler.add_job(process.crawl,'cron', args=[RooSpider], day='1', hour=8, minute=rand_min())
 
     ## On every day starting on the 2nd day of the month, enable scrapy deltafetch so that the scarper skips duplicates
-    scheduler.add_job(process.crawl, 'cron', args =[DeltaApaSpider], day= '2/1', hour=13, minute=20)
-    scheduler.add_job(process.crawl,'cron', args=[DeltaRooSpider], day= '2/1',  hour=13, minute=25)
+    scheduler.add_job(process.crawl, 'cron', args =[DeltaApaSpider], day= '2/1', hour=13, minute=rand_min())
+    scheduler.add_job(process.crawl,'cron', args=[DeltaRooSpider], day= '2/1',  hour=13, minute=rand_min())
 
     process1 = ArchiveProcess('apa')
     process2 = ArchiveProcess('roo')
 
     ## On every second day of the month, scrape archived HTML from the previous month into a .csv file
-    scheduler.add_job(process1.crawl, 'cron', args=[CLLSpider], kwargs = {"start_urls" :getFiles('apa')}, day = '2', hour=17, minute=00)
-    scheduler.add_job(process2.crawl, 'cron', args=[CLLSpider], kwargs = {"start_urls" :getFiles('roo')}, day = '2', hour=17, minute=15)
+    scheduler.add_job(process1.crawl, 'cron', args=[CLLSpider], kwargs = {"start_urls" :getFiles('apa')}, day = '2', hour=17, minute=rand_min())
+    scheduler.add_job(process2.crawl, 'cron', args=[CLLSpider], kwargs = {"start_urls" :getFiles('roo')}, day = '2', hour=17, minute=1rand_min())
     
     scheduler.start()
     process.start()
