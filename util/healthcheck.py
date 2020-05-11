1c1df641bbfd6268947209c4baaf252a07688183
irisgao713 <irisco226@yahoo.com>
2020-05-11 16:08:16 -0700

    check both apa and roo listings

--- util/healthcheck.py
+++ util/healthcheck.py
@@ -5,6 +5,10 @@ def check_rows(filename):
     '''
     Check if the CSV file contains more than 100 rows
     '''
-    df = pd.read_csv(filename)
-    if df.shape[0] < 100:
+
+    fold = "../results/parsed_raw/"
+
+    apa = pd.read_csv(fold + "apa/" + filename)
+    roo = pd.read_csv(fold + "roo/" + filename)
+    if apa.shape[0] < 100 or roo.shape[0] < 100:
         notify("contamination")
